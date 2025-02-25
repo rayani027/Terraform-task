1578c9a516c35181c1e37a5ed7477fcba8a81274
rayani027 <r.narendra7616@gmail.com>
2025-02-25 19:58:21 +0530

    Update variables.tf

---
+++ Terraform/Environments/Dev/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "The Azure region where resources will be created"
+  type        = string
+  default     = "East US"
+}
+
+variable "env" {
+  description = "The environment name"
+  type        = string
+  default     = "dev"
+}
